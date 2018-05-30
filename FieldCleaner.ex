b6821a8d629b62c45d9f05eb6410f3ff4f38e51b
Geordie443 <geordie191@gmail.com>
2018-05-30 16:53:04 -0400

    heavily changed the way the code workd, final or close

--- FieldCleaner.ex
+++ FieldCleaner.ex
@@ -1,40 +1,74 @@
 {:file, fileName} = {:file, "Fields.txt"}
 
-newList = File.read!(fileName)
-|> String.replace(".$","")
-|> String.replace(~r/(\d{2})(\: )/,"")
-|> String.replace(~r/(\d{1})(\: )/,"")
-|> String.replace(~r/(\n)(.*)(\n)/,"\n")
-|> String.replace(~r/(\n)/,",\n")
-|> String.replace(~r/(\n)/,"\n:")
+newList =
+  File.read!(fileName)
+  # get rid of dollar sign
+  |> String.replace(".$", "")
+  # remove 2 digit numbers followed by colon and space
+  |> String.replace(~r/(\d{2})(\: )/, "")
+  # remove 1 digit numbers followed by colon and space
+  |> String.replace(~r/(\d{1})(\: )/, "")
+  # remove blank lines
+  |> String.replace(~r/(\n)(.*)(\n)/, "\n")
+  # put commas at the end of each line  
+  |> String.replace(~r/(\n)/, ",\n-:")
+  # gets rid of colon at last line and comma ending last entry
+  |> String.slice(0..-5)
 
-listHiddenFields = ":" <> newList
-listFields = String.replace(listHiddenFields, ~r/(")/,"")
+# adds colon at start
+newList = ":" <> newList
 
+fieldList = String.split(newList, "-")
+listlen = length(fieldList)
 
-IO.puts "\nField Atoms:\n"
-IO.puts listFields
-IO.puts "\nHidden Field Atoms:\n"
-IO.puts listHiddenFields
+# cleaning the list to have no quotes on single word names
+cleanList = CleanFields.headFunction(fieldList, "")
+cleanList = cleanList <> ","
 
+# take user input for collection name
+# gets string and trims uneccesarry characters 
+collection =
+  IO.gets("Which collection is this for? :")
+  |> String.slice(0..-2)
 
-IO.puts '''
-\nDo you want to store these lists in files?
-Yes: y
-No: anything else
-'''
-userInput = IO.gets("Answer: ")
+fileOut = collection <> "_#{listlen}_Fields_Cleaned.txt"
+IO.puts("Cleaning Complete !")
+IO.puts("#{listlen} Schema Fields Cleaned\n")
+# output the cleaned list  to file
+File.write(fileOut, cleanList)
 
+# ----------------------------------------------------------------------------------
 
-case userInput do
-	"y\n" -> 
-		if (File.dir?("Field_Atoms") == false) do{
-			File.mkdir!("Field_Atoms")
-		}
-		end
-		File.write("./Field_Atoms/Field_Attoms.txt", listFields)
-		File.write("./Field_Atoms/Field_Attoms_Hidden.txt", listHiddenFields)
-		IO.puts("File Creation Complete")
+defmodule CleanFields do
+  def headFunction(fieldList, finalList) when length(fieldList) <= 1 do
+    [last] = fieldList
 
-	_ -> IO.puts("No Files Created")
+    finalList =
+      case String.contains?(last, ".") do
+        true ->
+          finalList <> last
+
+        false ->
+          noQuote = String.replace(last, ~r/(")/, "")
+          finalList <> noQuote
+      end
+
+    finalList
+  end
+
+  def headFunction(fieldList, finalList) do
+    [head | tail] = fieldList
+
+    finalList =
+      case String.contains?(head, ".") do
+        true ->
+          finalList <> head
+
+        false ->
+          noQuote = String.replace(head, ~r/(")/, "")
+          finalList <> noQuote
+      end
+
+    headFunction(tail, finalList)
+  end
 end
