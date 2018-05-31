650c521b7e55fdbefb47d3541c9778b20bd32f94
Geordie443 <geordie191@gmail.com>
2018-05-31 11:35:10 -0400

    added more regex functionality to account for bracket sections in larger lists

--- FieldCleaner.ex
+++ FieldCleaner.ex
@@ -1,28 +1,34 @@
+# make a file called Fields.txt with the schemaKeys from portal with 0: "_id" being the first line
+# put Fields.txt in the same directory that this .ex is in. 
+# run $ elixirc FieldCleaner.ex and give the name of the collection such as "Users"
+# file will be generated in a "Cleaned_Fields" directory
+
 {:file, fileName} = {:file, "Fields.txt"}
 
 newList =
   File.read!(fileName)
-  # get rid of dollar sign
+  # get rid of dollar signs
   |> String.replace(".$", "")
-  # remove 3 digit numbers followed by colon and space
-  |> String.replace(~r/(\d{3})(\: )/, "")
-  # remove 2 digit numbers followed by colon and space
-  |> String.replace(~r/(\d{2})(\: )/, "")
-  # remove 1 digit numbers followed by colon and space
-  |> String.replace(~r/(\d{1})(\: )/, "")
+  # remove 1-4 digit numbers followed by colon and space
+  |> String.replace(~r/(\d{1,4})(\: )/, "")
+  # replace brackets for collapsed sections
+  |> String.replace(~r/(\[)(.*)(\])(\n)/, "\n")
   # remove blank lines
   |> String.replace(~r/(\n)(.*)(\n)/, "\n")
-  # put commas at the end of each line  
+  # put dash and commas at start of each line for a split character
   |> String.replace(~r/(\n)/, ",\n-:")
+  # removes weird pattern
+  |> String.replace(~r/(\:)(\,)(\n)/, "")
   # gets rid of colon at last line and comma ending last entry
   |> String.slice(0..-5)
 
 # adds colon at start
 newList = ":" <> newList
 
+# split string to list on arbitrary dash
 fieldList = String.split(newList, "-")
+# generate length for filename purposes
 listlen = length(fieldList)
-
 # cleaning the list to have no quotes on single word names
 cleanList = CleanFields.headFunction(fieldList, "")
 cleanList = cleanList <> ","
@@ -45,7 +51,7 @@ end
 File.write(directory <> "/" <> fileOut, cleanList)
 
 # ----------------------------------------------------------------------------------
-
+# defmodule which cuts the list into [head|tail] and cleans each head individually then adds it to a list
 defmodule CleanFields do
   def headFunction(fieldList, finalList) when length(fieldList) <= 1 do
     [last] = fieldList
